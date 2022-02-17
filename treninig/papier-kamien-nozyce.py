e65e5bfa3f790e2bd00b5ec4a3ff08097ba99fe0
Dawid.S <dawid.baltica@gmail.com>
2022-02-17 15:51:29 +0100

    CodeWithMe 17.02.2022

--- treninig/papier-kamien-nozyce.py
+++ treninig/papier-kamien-nozyce.py
@@ -14,26 +14,30 @@ class Mark:
 while True:
     you: str = input('\nWybierz kamień, papier lub nożyce: ')
     computer: str = choice(('kamień', 'papier', 'nożyce'))
-    winner = (f'{Mark.YELLOW}REMIS{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})',
-              f'{Mark.GREEN}WYGRAŁEŚ{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})',
-              f'{Mark.RED}PRZEGRAŁEŚ{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})')
+    winner = {
+        0: f'{Mark.YELLOW}REMIS{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})',
+        1: f'{Mark.GREEN}WYGRAŁEŚ{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})',
+        2: f'{Mark.RED}PRZEGRAŁEŚ{Mark.RESET} {Mark.LIGHTGREY}({you} vs {computer}{Mark.RESET})'
+    }
 
     if you == computer:
         print(f'{winner[0]}')
-    elif you == 'nożyce' and computer == 'papier':
+
+    elif \
+            you == 'nożyce' and computer == 'papier' or \
+            you == 'kamień' and computer == 'nożyce' or \
+            you == 'papier' and computer == 'kamień':
         print(f'{winner[1]}')
-    elif you == 'kamień' and computer == 'nożyce':
-        print(f'{winner[1]}')
-    elif you == 'papier' and computer == 'kamień':
-        print(f'{winner[1]}')
-    elif you == 'kamień' and computer == 'papier':
+
+    elif \
+            you == 'kamień' and computer == 'papier' or \
+            you == 'papier' and computer == 'nożyce' or \
+            you == 'nożyce' and computer == 'kamień':
         print(f'{winner[2]}')
-    elif you == 'papier' and computer == 'nożyce':
-        print(f'{winner[2]}')
-    elif you == 'nożyce' and computer == 'kamień':
-        print(f'{winner[2]}')
+
     elif you == 'koniec':
         print(f'{Mark.BOLD}Koniec gry{Mark.RESET}')
         break
+
     else:
         print(f"{Mark.LIGHTGREY}Wpisz: 'paper', 'kamień', 'nożyce' lub 'koniec' aby zakończyć grę.{Mark.RESET}")
