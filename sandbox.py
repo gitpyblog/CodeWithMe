2ff171ce78a4eb805eb54a28fde5bacb2d2b6623
Dawid.S <dawid.baltica@gmail.com>
2022-02-21 13:38:35 +0100

    CodeWithMe 21.02.2022

--- sandbox.py
+++ sandbox.py
@@ -1,48 +1,10 @@
-# Napisz program, który utworzy dwa # X oraz Y, zbiór X zawiera a elementów, zbiór Y zawiera b elementów.
-# Wartości a oraz b to losowe liczby całkowite z przedziału <4,7>.
-# Zbiory powinny zawierać elementy o losowej wartości z przedziału <0,10>.
-# Następnie wykonaj następujące ćwiczenia:
-# 1. Wypisz na ekran informację czy zbiór X zawiera liczbę 5.
-# 2. Wypisz na ekran sumę zbiorów X oraz Y.
-# 3. Wypisz na ekran różnicę zbiorów X oraz Y.
-# 4. Wypisz na ekran różnicę zbiorów Y oraz X.
-# 5. Wypisz na ekran wartość najwyższego elementu w obu zbiorach.
-# 6. Usuń ze zbioru X pierwszy element i dołącz go do zbioru Y.
-# 7. Przekopiuj wszystkie elementy zbioru X do zbioru Y.
-# 8. Wypisz na ekran część wspólną zbiorów X oraz Y.
+NAME = 'Misiu'
+
+def przedstaw_sie(name):
+    return f'Cześć miło Cię widzieć {name}'
 
 
-from random import sample, randint
-
-a = randint(4, 7)
-b = randint(4, 7)
-X = set(sample(range(10), a))  # sample określa ile razy ma powtarzać losowanie sample('przedział', ilość powtórzeń)
-Y = set(sample(range(10), b))
-
-
-def zad1(x):  # 1. Wypisz na ekran informację czy zbiór X zawiera liczbę 5.
-    if 5 in x:
-        return f'Zad1: 5 zwiera się w X: {X}'
-    else:
-        return f'Zad1: 5 nie zawiera się X: {X}'
-
-
-def zad2(x, y):  # 2. Wypisz na ekran sumę zbiorów X oraz Y
-    return f'Zad2: Suma zbiorów X i Y to: {sum(x.union(y))} {x.union(y)}'
-
-
-def zad3(x, y):  # 3-4. Wypisz na ekran różnicę zbiorów X i Y oraz Y i X.
-    return f'Zad3, Zad4: Różnica X-Y oraz Y-X: {x.symmetric_difference(y)}'
-
-
-def zad5():  # 5. Wypisz na ekran wartość najwyższego elementu w obu zbiorach.
-    pass
-
-
-print(f'X: {X}  a:{a}')
-print(f'Y: {Y}  b:{b}')
-
-print('')
-print(zad1(X))  # zadnie 1
-print(zad2(X, Y))  # zadnie 2
-print(zad3(X, Y))  # zadnie 3 i 4
+print(przedstaw_sie('Ania'))
+print(przedstaw_sie('Adam'))
+print(przedstaw_sie('Misiu'))
+print(przedstaw_sie(NAME))
