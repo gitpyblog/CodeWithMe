357c521f16ca3dfeaa0b78177f1b45be0d88a65d
Dawid.S <dawid.baltica@gmail.com>
2022-02-22 10:03:38 +0100

    CodeWithMe 22.02.2022 - start

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,2 @@
-NAME = 'Misiu'
+#  1. Napisz funkcję Pythona, aby znaleźć maksymalną liczbę trzech liczb.
 
-def przedstaw_sie(name):
-    return f'Cześć miło Cię widzieć {name}'
-
-
-print(przedstaw_sie('Ania'))
-print(przedstaw_sie('Adam'))
-print(przedstaw_sie('Molek'))
-print(przedstaw_sie(NAME))
