f5ead6ecbfc17a2aa2828ad89fa2195737122d1b
Dawid.S <dawid.baltica@gmail.com>
2022-02-23 21:36:23 +0100

    23.02.2022

---
+++ programy/is_even.py
@@ -0,0 +1,14 @@
+def is_even(number: int) -> bool:
+    """Funkcja sprawdza, czy podana liczba jest parzysta
+
+    Args:
+        number (int | float): liczba, którą weryfikujemy
+
+    Returns:
+        bool: zwraca True, jeśli liczba jest parzysta
+    """
+
+    if type(number) is not int:
+        raise TypeError("Niepoprawna liczba")
+
+    return True if number % 2 == 0 else False
