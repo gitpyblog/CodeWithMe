d930a61899d28052a19982c2cbd9e82b86202026
Dawid.S <dawid.baltica@gmail.com>
2022-02-23 12:37:50 +0100

    CodeWithMe 23.02.2022

---
+++ programy/studenci.py
@@ -0,0 +1,20 @@
+def student(imie, nazwisko, semestr, *wykaz_przedmiotow, stypendium=False):
+    def sprawdz_stypendium(x):
+        if x is True:
+            return 'Przyznane'
+        else:
+            return 'Nieprzyznane'
+
+    return (
+        f'🟢 Cześć {imie} {nazwisko}, '
+        f'jesteś studentem {semestr} semestru, '
+        f'Twój wykaz przedmiotów: {", ".join(wykaz_przedmiotow)}. '
+        f' Stypendium: {sprawdz_stypendium(stypendium)}')
+
+
+sebastian = student('Sebastian', 'Kowalski', 2, 'matematyka', 'fizyka', 'biologia', 'chemia')
+iwona = student('Iwona', 'Kowalska', 3, 'zarządzanie', 'python', 'filozofia', stypendium=True)
+dawid = student('Dawid', 'Kowalski', 3, 'python', 'sql', 'matematyka')
+zofia = student('Zosia', 'Tolar', 4, 'historia', 'j. angielski', 'filozofia')
+
+print(f' {sebastian},\n {iwona},\n {dawid},\n {zofia},')
