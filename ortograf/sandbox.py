2879e123da711710c66891e3cb186613e0d9ca9d
Dawid.S <dawid.baltica@gmail.com>
2022-02-24 08:53:51 +0100

    24.02.2022

--- ortograf/sandbox.py
+++ ortograf/sandbox.py
@@ -16,3 +16,15 @@
 #
 
 
+import sys
+
+sys.tracebacklimit = 0
+
+# .mro() (method resolution order) - pokazuje kolejność rozwiązywania metod
+# isinstance(arg, typ) - sprawdza, czy argument jest danego typu
+# mypy — tool do walidacji
+# gradual typing — łączy zalety typowania statycznego i dynamicznego
+
+
+# Type annotation /Type hinting. Podpowiedzi typów (od python 3.5)
+# To nie jest mechanizm walidacji, to jest tylko mechanizm podpowiedz
