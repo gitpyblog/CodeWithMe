7d8e891d60769cb573b8f9de04a7b4794b882445
Dawid.S <dawid.baltica@gmail.com>
2022-03-04 09:09:43 +0100

    CodeWithMe 04.03.2022

--- programy/waluty.py
+++ programy/waluty.py
@@ -2,11 +2,10 @@ import requests
 
 response_json = requests.get('https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json').json()
 
-print(f'\nOdpowiedź z https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json:\n{response_json}\n')
-
 ask = response_json['rates'][0]['ask']
 bid = response_json['rates'][0]['bid']
 
+print(f'\nOdpowiedź z https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json:\n{response_json}\n')
 print(f"Aktualny kurs {response_json['currency']} na dzień {response_json['rates'][0]['effectiveDate']}:")
 print(f"Sprzedaż: {ask} zł")
 print(f"Skup: {bid} zł")
