bae1cdf385e2975e278886a65f6468371b875b41
Dawid.S <dawid.baltica@gmail.com>
2022-03-04 15:35:27 +0100

    homework update

--- programy/waluty.py
+++ programy/waluty.py
@@ -1,28 +1,7 @@
 import requests
 
-# jaka_waluta = input('Jaka walutę chcesz sprawdzić? ')
-# kurs_waluty = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/{jaka_waluta}/?format=json').json()
-#
-# ask = kurs_waluty['rates'][0]['ask']
-# bid = kurs_waluty['rates'][0]['bid']
-
-# print(f'\nOdpowiedź z API https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json:\n{kurs_waluty}\n')
-# print(f"Aktualny kurs {kurs_waluty['currency']} na dzień {kurs_waluty['rates'][0]['effectiveDate']}:")
-# print(f"Sprzedaż: {ask} zł")
-# print(f"Skup: {bid} zł")
-
-# http://api.nbp.pl/api/cenyzlota/?format=json
-
-# kurs_zlota = requests.get('http://api.nbp.pl/api/cenyzlota/?format=json').json()
-
-
-# print(f'answer{kurs_zlota}')
-# print(kurs_zlota_uncja)
-
-
-#####################################################
-kurs_zlota_json = requests.get('http://api.nbp.pl/api/cenyzlota/?format=json').json()
-kurs_zlota_uncja = kurs_zlota_json[0]['cena'] * 32.1
+kurs_au_json = requests.get('https://api.nbp.pl/api/cenyzlota/?format=json').json()
+kurs_au_uncja = kurs_au_json[0]['cena'] * 32.1
 
 kurs_dolara_json = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json').json()
 kurs_dolara = kurs_dolara_json['rates'][0]['bid']
@@ -41,10 +20,10 @@ Iwona = {
     'au': 5
 }
 
-portfel = (Iwona['usd'] * kurs_dolara) +\
-          (Iwona['chf'] * kurs_frank) +\
-          (Iwona['eur'] * kurs_euro) +\
-          (Iwona['au'] * kurs_zlota_uncja)\
+portfel = (Iwona['usd'] * kurs_dolara) + \
+          (Iwona['chf'] * kurs_frank) + \
+          (Iwona['eur'] * kurs_euro) + \
+          (Iwona['au'] * kurs_au_uncja) \
           + Iwona['pln']
 
 print(f'Łączna wartość w złotych Twojego portfela na dzień dzisiejszy to: {portfel} zł')
