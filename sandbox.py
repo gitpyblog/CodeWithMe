ff2a73d06a50795be47f302cdd8bfc5b3d85d11f
Dawid.S <dawid.baltica@gmail.com>
2022-03-04 09:13:52 +0100

    CodeWithMe 04.03.2022

---
+++ sandbox.py
@@ -0,0 +1,17 @@
+Ludzie = {
+    'Adam': {
+        'imie': 'Adam',
+        'nazwisko': 'Kowalski',
+        'wiek': 25
+    },
+    'Ola': {
+        'imie': 'Ola',
+        'nazwisko': 'Kowalska',
+        'wiek': 19
+    },
+    'Janek': {
+        'imie': 'Janek',
+        'nazwisko': 'Kowalski',
+        'wiek': 17
+    }
+}
