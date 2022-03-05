c7f0d5ce2bec6506a0e2c76a9080f1ca0bb56146
IWONA <magicbox.lgn@gmail.com>
2022-03-05 14:08:34 +0100

    wszystkie zadania wykonano :-)

--- programy/waluty.py
+++ programy/waluty.py
@@ -19,6 +19,39 @@ import requests
 # print(f'answer{kurs_zlota}')
 # print(kurs_zlota_uncja)
 
+# Zadanie 1:
+# Zaokrągli kotwę podsumowania portfela do 2 cyfr po przecinku
+# print(round('portfel', 2))
+
+# Zadanie 2:
+# Zmieniono strukturę słownika z danymi portfela na następującą:
+
+
+#dostosuj program do nowej struktury
+# Iwona = {
+#    'pln': 10000,
+ #   'usd': 3000,
+  #  'chf': 1000,
+  #  'eur': 5000,
+   # 'au': 5
+# }
+
+#Zadanie 3:
+#Dodaj wyświetlanie danych dla więcej niż jednego portfela:
+
+#Portfel = {
+#   'Iwona': {
+#       'imie': 'Iwona',
+#       'nazwisko': 'Langner',
+#       'data_utworzenia_portfela': '04.03.2022',
+#       'waluty': {
+#           'pln': 10000,
+#           'usd': 3000,
+#          'chf': 1000,
+#           'eur': 5000,},
+#       'metale': {
+#           'au': 5}
+#   },
 
 #####################################################
 kurs_zlota_json = requests.get('http://api.nbp.pl/api/cenyzlota/?format=json').json()
@@ -34,17 +67,41 @@ kurs_frank_json = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/ch
 kurs_frank = kurs_frank_json['rates'][0]['bid']
 
 Iwona = {
-    'pln': 10000,
-    'usd': 3000,
-    'chf': 1000,
-    'eur': 5000,
-    'au': 5
+   'imie': 'Iwona',
+    'nazwisko': 'Langner',
+    'data_utworzenia_portfela': '04.03.2022',
+    'waluty': {
+        'pln': 10000,
+        'usd': 3000,
+        'chf': 1000,
+        'eur': 5000,},
+    'metale': {
+       'au': 5}
 }
 
-portfel = (Iwona['usd'] * kurs_dolara) +\
-          (Iwona['chf'] * kurs_frank) +\
-          (Iwona['eur'] * kurs_euro) +\
-          (Iwona['au'] * kurs_zlota_uncja)\
-          + Iwona['pln']
+portfel_Iwony = (Iwona['waluty']['usd'] * kurs_dolara) +\
+          (Iwona['waluty']['chf'] * kurs_frank) +\
+          (Iwona['waluty']['eur'] * kurs_euro) +\
+          (Iwona['metale']['au'] * kurs_zlota_uncja)\
+          + Iwona['waluty']['pln']
 
-print(f'Łączna wartość w złotych Twojego portfela na dzień dzisiejszy to: {portfel} zł')
+
+Dawid = {
+'imie': 'Dawid',
+'nazwisko': 'Szatkowski',
+'data_utworzenia_portfela': '05.03.2022',
+'waluty': {
+    'pln': 0,
+    'usd': 5000},
+'metale': {
+    'au': 20}
+}
+portfel_Dawida = (Dawid['waluty']['usd'] * kurs_dolara) +\
++ Dawid['waluty']['pln']
+(Dawid['metale']['au'] * kurs_zlota_uncja)\
+
+print(f'Łączna wartość w złotych portfela Iwony na dzień dzisiejszy to: {portfel_Iwony} zł')
+print(round(portfel_Iwony, 2))  # zad. 1
+print(f'Łączna wartość w złotych portfela Dawida na dzień dzisiejszy to: {portfel_Dawida} zł')
+print(round(portfel_Dawida, 2))
+
